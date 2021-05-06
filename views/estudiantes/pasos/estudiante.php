75b79f8e50fc6710d3077287e4e18ca8f166d54e
= <Freddypimpns@gmail.com>
2021-05-06 01:52:32 -0400

    cambios en el formulario

---
+++ views/estudiantes/pasos/estudiante.php
@@ -0,0 +1,77 @@
+
+
+<div id="form-estudiantes" style="background-color: white; margin-top:50px; width:60%; padding:20px; margin-left:24%;">
+    <div>
+    <form action="" method="post">
+      <h5> Datos del estudiante </h5> 
+        <div class="row">
+            <div class="col-md-16"><br>
+                <input class="form-control" type="text" name="" placeholder="Nombre del estudiante" id="" require>
+            </div>
+            <div class="col-md-6"><br>
+                <input class="form-control" type="Text" placeholder="Apellido paterno" name="" id="" require>
+            </div>
+            <div class="col-md-6"><br>
+                <input class="form-control" type="Text" placeholder="Apellido materno" name="" id="" require>
+            </div>
+            <div class="col-md-6"><br>
+                <input class="form-control" type="text" placeholder="Colegio anterior" name="" id="" require>
+            </div>
+            <div class="col-md-6"><br>
+                <input class="form-control" type="text" placeholder="Curso anterior" name="" id="" require>
+            </div>
+            <div class="col-md-6"><br>
+                <label for="">
+                    Seleccione una provincia</label>
+            <select class="form-control" name="meal_preference" id="meal_preference"    >
+                <option value="Santo Domingo">Provincias</option>
+                <option value="Santo Domingo">Santo Domingo</option>
+                <option value="Distrito Nacional">Distrito Nacional</option>
+                <option value="Santiago">Santiago</option>
+                <option value="San Cristóbal">San Cristóbal</option>
+                <option value="La Vega">La Vega</option>
+                <option value="Puerto Plata">Puerto Plata</option>
+                <option value="San Pedro de Macorís">San Pedro de Macorís</option>
+                <option value="Duarte">Duarte</option>
+                <option value="La Altagracia">La Altagracia</option>
+                <option value="La Romana">La Romana</option>
+                <option value="San Juan">San Juan</option>
+                <option value="Espaillat">Espaillat</option>
+                <option value="Azua">Azua</option>
+                <option value="Barahona">Barahona</option>
+                <option value="Monte Plata">Monte Plata</option>
+                <option value="Peravia">Peravia</option>
+                <option value="Monseñor Nouel">Monseñor Nouel</option>
+                <option value="Valverde">Valverde</option>
+                <option value="Sánchez Ramírez">Sánchez Ramírez</option>
+                <option value="María Trinidad Sánchez">María Trinidad Sánchez</option>
+                <option value="Montecristi">Montecristi</option>
+                <option value="Samaná">Samaná</option>
+                <option value="Bahoruco">Bahoruco</option>
+                <option value="Hermanas Mirabal">Hermanas Mirabal</option>
+                <option value="El Seibo">El Seibo</option>
+                <option value="Hato Mayor">Hato Mayor</option>
+                <option value="Dajabón">Dajabón</option>
+                <option value="Elías Piña">Elías Piña</option>
+                <option value="San José de Ocoa">San José de Ocoa</option>
+                <option value="Santiago Rodríguez">Santiago Rodríguez</option>
+                <option value="Independencia">Independencia</option>
+                <option value="Pedernales">Pedernales</option>
+            </select>
+            </div>
+            <div class="col-md-6"><br>
+            <label for="">Fecha de Nacimiento</label>
+                <input class="form-control" type="Date" placeholder="000-000-0000" name="" id="" require>
+            </div>
+            <div class="col-md-12"><br>
+            <label for="">Motivo de salida</label>
+            <textarea class="form-control" placeholder="Razon de salida de la institucion anterior" name="" id="" cols="10" rows="4"></textarea>
+            </div>
+            <div class="col-md-12"><br>
+                <a class="btn btn-danger" id="siguiente1">Siguiente</a>
+            </div>
+        </form>
+        </div>
+    </div>
+</div>
+            
---
+++ views/estudiantes/pasos/padres.php
@@ -0,0 +1,43 @@
+
+<div id="form-estudiantes" style="background-color: white; margin-top:50px; width:60%; padding:20px; margin-left:24%;">
+    <div>
+    <form action="" method="post">
+      <h5> Datos de tutores </h5> 
+        <div class="row">
+            <div class="col-md-12"><br>
+                <input class="form-control" type="text" name="" placeholder="Nombre del padre" id="" require>
+            </div>
+            <div class="col-md-12"><br>
+                <input class="form-control" type="Text" placeholder="Nombre de la madre" name="" id="" require>
+            </div>
+            <div class="col-md-6"><br>
+                <label for="">Telefono de la madre</label>
+                <input class="form-control" type="Text" placeholder="000-000-000" name="" id="" require>
+            </div>
+            <div class="col-md-6"><br>
+                <label for="">Telefono de la padre</label>
+                <input class="form-control" type="text" placeholder="000-000-000" name="" id="" require>
+            </div>
+            <div class="col-md-6"><br>
+                <label for=""><label for="">Celular de la padre</label></label>
+                <input class="form-control" type="text" placeholder="000-000-0000" name="" id="" require>
+            </div>
+            <div class="col-md-6"><br>
+              <label for="">Celular de la madre</label>
+                <input class="form-control" type="Text" placeholder="000-000-0000" name="" id="" require>
+            </div>
+
+            <div  class="col-md-12"><br>
+                <textarea class="form-control"  placeholder="Direccion" name="" id="" cols="30" rows="4"></textarea>
+            </div>
+            <div class="col-md-6"><br>
+                <a   class="btn btn-primary " id="anterior1">Anterior</a>
+            </div>
+            <div class="col-md-6"><br>
+                <a  style="float: right;"class="btn btn-danger " id="siguiente2">Siguiente</a>
+            </div>
+            
+        </form>
+        </div>
+    </div>
+</div>
