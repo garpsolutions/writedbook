e10cc30283803e60c00e00e542e299e4b2b674ea
Neitan Garcia Saviñon <neitangs@gmail.com>
2021-05-10 16:21:15 -0400

    cambios a docentes

---
+++ views/docentes/detalles_docentes.php
@@ -0,0 +1,197 @@
+<?php 
+include("../menu_lateral.php"); 
+?>
+<div id="perfil_docentes" class="row" >
+    <div style="background-color: white; width:600px; margin-top:50px;padding:40px; margin-left:8%;">
+        <div class="row">
+            <div class="col-md-6" style="border-right:solid 3px gray;">
+                <div>
+                    <img src="../../img/pp.jpg"class="img-fluid" width="300px" style="border:solid 5px gray; border-radius:175px" alt=""><br><br>
+                    <center>
+                        Freddy Miguel Pereyra, 4to C <br>
+                        <strong> Activo </strong> 
+                    </center>
+                    
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div>
+                    <div>
+                        <button class="btn btn form-control" id="indice"style="background-color: rgb(11, 11, 53);color:white">Cantidad de Asiganaturas Impartidas</button>
+                    </div>
+                    <div> <br>
+                        <a href="horario_docente.php"><button class="btn btn form-control" style="background-color: rgb(11, 11, 53);color:white"> Horario</button></a>
+                    </div>
+                    <div><br>
+                        <button class="btn btn form-control" id="informacion" style="background-color: rgb(11, 11, 53);color:white">Informacion</button>
+                    </div>
+                    <div><br>
+                        <a href="record_notas.php"><button class="btn btn form-control" style="background-color: rgb(11, 11, 53);color:white">Reconocimiento</button></a>
+                    </div>
+                    <div><br>
+                        <a href="record_notas.php"><button class="btn btn form-control" style="background-color: rgb(11, 11, 53);color:white">Maestros Suplentes</button></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div id="informacion">
+<div id="estudiante">
+    <?php
+        include("paso_modificar/estudiante.php"); 
+    ?>
+</div>
+<div id="padres">
+    <?php
+        include("paso_modificar/padres.php");
+    ?>
+</div>
+<div id="cercano_madre">
+    <?php
+        include("paso_modificar/cercanos_madre.php");
+    ?>
+</div>
+<div id="cercano_padre">
+    <?php
+        include("paso_modificar/cercanos_padre.php");
+    ?>
+</div>
+<div id="documentos">
+    <?php
+        include("paso_modificar/documentos.php");
+    ?>
+</div>
+
+
+<script>
+//formulario disabled
+    $(".campo").attr("disabled","true");
+//formulario activo
+    $(".editar").click(function(){
+        alert("fgdfgh");
+    });
+
+//formularios inicialmente ocultos
+    $("#padres").hide();
+    $("#cercano_madre").hide();
+    $("#cercano_padre").hide();
+    $("#documentos").hide();
+    $("#estudiante").hide();
+//Mostrar padres
+    $("#informacion").click(function(){
+        $("#perfil_estudiante").hide();
+        $("#estudiante").show();
+    });
+    $("#siguiente1").click(function(){
+        $("#padres").animate({
+            opacity: '1',
+        });
+        $("#estudiante").animate({
+      opacity: '0.5',
+    });
+        $("#estudiante").hide(300);
+        $("#padres").show();    
+    });
+//regresar a estudiante
+    $("#anterior1").click(function(){
+        $("#estudiante").animate({
+            opacity: '1'
+        });
+
+        $("#padres").animate({
+      opacity: '0.5',
+    });
+        $("#padres").hide(300);
+        $("#estudiante").show();    
+    });
+    
+//familiares cercanos a la madre
+    $("#siguiente2").click(function(){
+        $("#cercano_madre").animate({
+            opacity: '1',
+        });
+        $("#padres").animate({
+      opacity: '0.5',
+    });
+        $("#padres").hide(300);
+        $("#cercano_madre").show();    
+    });
+
+// regresar a padres
+
+$("#anterior2").click(function(){
+        $("#padres").animate({
+            opacity: '1'
+        });
+
+        $("#cercano_madre").animate({
+      opacity: '0.5',
+    });
+        $("#cercano_madre").hide(300);
+        $("#padres").show();    
+ });
+
+ // Familiar cercano padre
+ $("#siguiente3").click(function(){
+        $("#cercano_padre").animate({
+            opacity: '1',
+        });
+        $("#cercano_madre").animate({
+      opacity: '0.5',
+    });
+        $("#cercano_madre").hide(300);
+        $("#cercano_padre").show();    
+});
+
+//regresar a familiar cercano a la madre
+$("#anterior3").click(function(){
+        $("#cercano_madre").animate({
+            opacity: '1'
+        });
+
+        $("#cercano_padre").animate({
+      opacity: '0.5',
+    });
+        $("#cercano_padre").hide(300);
+        $("#cercano_madre").show();    
+ });
+
+
+ // Documentos
+ $("#siguiente4").click(function(){
+        $("#documentos").animate({
+            opacity: '1',
+        });
+        $("#cercano_padre").animate({
+      opacity: '0.5',
+    });
+        $("#cercano_padre").hide(300);
+        $("#documentos").show();    
+});
+
+$("#anterior4").click(function(){
+        $("#cercano_padre").animate({
+            opacity: '1'
+        });
+
+        $("#documentos").animate({
+      opacity: '0.5',
+    });
+        $("#documentos").hide(300);
+        $("#cercano_padre").show();    
+ });
+
+$("#indice").click(function(){
+    Swal.fire({
+        title: 'El índice academico del estudiante es: 5',
+        showClass: {
+        popup: 'animate__animated animate__fadeInDown'
+        },
+        hideClass: {
+        popup: 'animate__animated animate__fadeOutUp'
+        }
+    });
+});
+
+</script>
---
+++ views/docentes/horario_docente.php
@@ -0,0 +1,44 @@
+<?php 
+include("../menu_lateral.php");
+?>
+<div id="form-estudiantes" style="background-color: white; margin-top:50px; width:60%; padding:20px; margin-left:24%;">
+<h3>Horario del Docente</h3> <br>
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Asignatura</th>
+      <th scope="col">Tanda</th>
+      <th scope="col">Horas</th>
+      <th scope="col">Aula</th>
+      <th scope="col">Dias</th>
+    </tr>
+  </thead>
+  <tbody>
+  
+    <tr>
+      <td scope="row">Matematica</td>
+      <td>Mañana</td>
+      <td>9:00 AM - 11:00 AM</td>
+      <td>1ro C</td>
+      <td>Lunes</td>
+    </tr> 
+  
+    
+    <tr>
+      <td scope="row">Fisica</td>
+      <td>Vespertina</td>
+      <td>2:00 PM - 4:00 PM </td>
+      <td>1ro C</td>
+      <td>Martes</td>
+
+    </tr>
+    <tr>
+      <td scope="row">3</td>
+      <td >Larry the Bird</td>
+      <td>@twitter</td>
+      <td>1ro C</td>
+      <td>Martes</td>
+    </tr>
+  </tbody>
+</table>
+</div>
